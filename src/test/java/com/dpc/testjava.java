0b7456c55e891f1218838ae67dc71ffa69d45cd6
dpc <deric.zhu@gmail.com>
2020-04-01 16:43:14 +0800

    testmvn2github

---
+++ src/test/java/com/dpc/testjava.java
@@ -0,0 +1,22 @@
+package com.dpc;
+
+import org.junit.Test;
+
+/* dpc-20200330*/
+/* dpc-20200401*/
+public class testjava {
+
+    @Test
+   public void test1(){
+        System.out.println("iamdpctest");
+        System.out.println();
+
+//        abcDef
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("iamdpctest");
+        System.out.println();
+    }
+}
